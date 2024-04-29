588d24d8644f962afbe4f37d89c8e1bde74c6266
ellafsd <ellagirin82@gmail.com>
2024-04-29 09:30:39 -0400

    conflict solved

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,89 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create a function to initialize app
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: "Enter your GitHub username:",
+        default: "GitHubUser"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter your email address:",
+        default: "ellagirin@gmail.com"
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter your project's title:",
+        default: "My Awesome Project"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Write a short description of your project:",
+        default: "A brief description of my project"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose a license for your project:",
+        choices: ['MIT', 'APACHE 3.0', 'GPL v3.0', 'BSD 3-Clause', 'None'],
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What command should be run to install dependencies?",
+        default: "npm install"
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "What command should be used to run tests?",
+        default: "npm test"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What should users know about using the repository?",
+        default: "Instructions on usage"
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "Who contributed to this project?",
+        default: "Your collaborator's names"
+    }
+];
+
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+            return;
+        }
+        console.log("Successfully wrote to", fileName);
+    });
+}
+
+// Create a function to initialize app
 function init() {}
+console.log("Welcome to the README Generator");
+inquirer.prompt(questions).then((answers) => {
+    console.log("Generating your README...");
+    const markdownContent = generateMarkdown(answers);
+    writeToFile("README.md", markdownContent);
+});
+
 
 // Function call to initialize app
-init();
+  init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,68 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Create a function that returns a license badge based on which license is passed in.
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  const badges = {
+      'MIT License': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+      'Apache License 2.0': '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+      'GNU General Public License v3.0': '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+      'None': ''
+  };
+  if (badges[license]) {
+    return badges[license];
+  } else {
+    return '';
+  }
 }
+
+
+// Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `This project is licensed under the ${license}. ${renderLicenseLink(license)}`;
+    }
+}
+
+
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  To run tests, use the following command:
+  \`\`\`
+  ${data.tests}
+  \`\`\` 
+  `;}
 
 module.exports = generateMarkdown;
